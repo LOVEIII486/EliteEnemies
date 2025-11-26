96b8c4cec4c6ec691abb2260b05d0bc7782e971f
Setekh <setekh4164@gmail.com>
2025-11-26 17:56:37 +0800

    微调几个词条的数值

--- EliteEnemy/AffixBehaviors/Behaviors/SlimeBehavior.cs
+++ EliteEnemy/AffixBehaviors/Behaviors/SlimeBehavior.cs
@@ -15,7 +15,7 @@ namespace EliteEnemies.EliteEnemy.AffixBehaviors.Behaviors
         private static readonly float InitialScale       = 3.5f;   // 初始体型倍率
         private static readonly float MinScale           = 0.5f; // 最小体型倍率
         private static readonly float InitialHealthMult  = 3.5f;   // 初始血量倍率
-        private static readonly float InitialDamageMult  = 0.4f; // 初始伤害倍率
+        private static readonly float InitialDamageMult  = 0.65f; // 初始伤害倍率
         private static readonly float MaxDamageMult      = 1.6f; // 最大伤害倍率
         private static readonly float HealthThreshold    = 0.05f; // 血量变化达到 5% 才更新
 
